1ccb8dd94a52da38213e217c151e806ec96086a3
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2022-07-27 20:07:04 -0300

    Login Page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
