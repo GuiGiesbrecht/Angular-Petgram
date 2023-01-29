b5c45760efe2dd3bc5084a2f39f0994d6f36487f
Guilherme Duarte Giesbrecht <guidgiesbrecht@gmail.com>
2023-01-29 12:44:50 -0300

    Final Login with header and footer to app

--- petgram/src/app/app.module.ts
+++ petgram/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterModule } from './components/footer/footer.module';
+import { HeaderModule } from './components/header/header.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FooterModule, HeaderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
